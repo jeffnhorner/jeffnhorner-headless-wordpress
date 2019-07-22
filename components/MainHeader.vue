15d75b16addd95b955c2ac3df5728c690d84f76a
Jeff Horner <jhorner3208@gmail.com>
2019-07-21 21:14:48 -0500

    created base portfolio pages, added stylelint config, updated eslint and prettier configs

---
+++ components/MainHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <MainNavigation :class="$style.class" />
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            data: [],
+        }),
+    };
+</script>
+
+<style lang="scss" scoped>
+    .class {
+        display: block;
+    }
+</style>
